ca98b00cc9d84679e2c61ab83c8f4a89e6d84adc
cute90210 <marceloah9199@gmail.com>
2023-11-10 23:03:38 -0500

    chore: initialize app structure and routing
    - Use App component as main entry point to display homepage component.
    - Wrap Home component with product context provider.
    
    - Add routing by using react-router-dom.
    - Create routes to the main page and store page in main.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from './Pages/Home'
+import { ProductProvider } from './Services/ProductContext'
+import "./index.css"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <ProductProvider>
+        <Home></Home>
+      </ProductProvider>
     </>
   )
 }
