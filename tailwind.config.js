d0d680db09df3c789a85137a5a5dd1199dedd03b
cute90210 <marceloah9199@gmail.com>
2023-11-14 14:18:54 -0500

    base

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const plugin = require('tailwindcss/plugin');
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(({ matchUtilities }) => {
+      matchUtilities({
+        perspective: (value) => ({
+          perspective: value,
+        }),
+      });
+    }),
+  ],
+}
+
